7f5cf167029717b7f5a9274361c94a959e391dce
Chaima <amirou.septiembre@gmail.com>
2024-10-03 16:08:56 +0200

    clase o oy q se

--- app/src/main/java/com/example/moviles1apuntes/MainActivity.kt
+++ app/src/main/java/com/example/moviles1apuntes/MainActivity.kt
@@ -3,13 +3,101 @@ package com.example.moviles1apuntes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity() {
+    val bebida = Bebida("fotos/coca_cola.jpg", 3.0f, "CocaCola Normal", 0.5f, false)
+    val c1 = Bebida("fotos/marinera.jpg", 2.50f, "maaaaaar", 0.5f, false)
+    val c2 = Bebida("fotos/combinado1.jpg", 3.0f, "menu 1", 0.5f, false)
+    val c3 = Bebida("fotos/tartaQueso.jpg", 3.0f, "tartita", 0.5f, false)
+    val comanda = Comanda(2)
+
+    println("Comanda: " $co)
 }
 
+//CLASE
+//quitamos data porque no es compatible con abstract
+abstract class Producto (open val foto: String = "", open var precio: Float = 0.0f, open val descripcion:String ="")
+{
+    constructor(p:Producto): this(p.foto,p.precio, p.descripcion )
+
+    companion object{ //para que sean estaticos
+        val IVA = 1.21f
+        val IVA_REDUCIDO = 1.1f
+    }
+
+}
+
+//----------------------------------------------------------------------------
+data class Bebida(override val foto: String = "", override var precio: Float = 0.0f, override val descripcion:String ="" ,  val tamLitros:Float=3.33f, val alcoholica:Boolean = false):Producto(foto, precio, descripcion)//para llamar alconstructor de la clase producto
+{
+    init{
+        precio = when(alcoholica)
+        {
+            false-> this.precio * IVA_REDUCIDO
+            true -> this.precio * IVA
+        }
+    }
+}
+//----------------------------------------------------------------------------
+enum class TipoComida{ //enumerado
+    ENTRANTE, PRINCIPAL, POSTRE
+}
+//----------------------------------------------------------------------------
+data class Comida(override val foto: String, override var precio: Float, override val descripcion:String , val tipoComida: TipoComida= TipoComida.ENTRANTE):Producto(foto, precio, descripcion)//para llamar alconstructor de la clase producto
+{
+
+}
+
+//----------------------------------------------------------------------------
+//lo que se quiere es inicializar siempre la lista vacia
+data class Comanda(var numMesa:Int =1, var numComensales:Int=1)//esto actua como constructor primario
+//pero alli solo se puede inicializar las variables
+{
+    var listaProductos: MutableList<Producto> = mutableListOf()
+
+    init {//cada vez que llames al constructor primario ejecuta el init despues de el
+        listaProductos = mutableListOf()
+    }
+
+    fun añadirProducto(p:Producto)
+    {
+        listaProductos.add(p)
+    }
+
+    fun eliminarProductos(p:Producto)
+    {
+        listaProductos.remove(p)
+    }
+
+}
+//----------------------------------------------------------------------------
+//completar con el cod de clase, que lo va a subir
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* COSAS HECHAS EN CASA
 open class Producto{//open para que se pueda heredar de la clase
     //declaro las variables
     protected var foto: String = ""
@@ -134,3 +222,5 @@ class Comanda{
     }
 
 }
+
+ */
